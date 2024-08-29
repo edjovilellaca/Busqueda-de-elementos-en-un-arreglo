3e5e8cd50e84fe6108f53428ef6a4d74c017a87c
Joel/Zakloni <edjovilellaca@ittepic.edu.mx>
2024-08-28 20:03:52 -0700

    Ya quedó

---
+++ script.js
@@ -0,0 +1,5 @@
+const nombres = ["Tania", "Hannia", "Mariana", "Yazmin"];
+let oracion = "Pepe";
+
+if (nombres.includes(oracion)) console.log("El nombre que coincide con el arreglo es: " + oracion);
+else console.log("No hay coincidencias");
